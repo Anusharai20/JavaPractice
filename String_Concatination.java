94b8d12645b73f13d0f9d4cb2f018b2de4949875
Anusha Rai <118553742+Anusharai20@users.noreply.github.com>
2023-03-02 22:08:49 +0545

    Add files via upload

---
+++ String_Concatination.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Concatination here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class String_Concatination
+{
+    public static void main(String[] args)
+   {
+       String a = "Hello!";
+       String b = " I am Anusha.";
+       //Using the operator +
+       String c = a + b;
+       //Displaying the result
+       System.out.println(c);
+   }
+}
