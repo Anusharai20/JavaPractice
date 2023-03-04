90faf1fe2d4673204ad214dd76a1beeac25ed720
Anusha Rai <118553742+Anusharai20@users.noreply.github.com>
2023-03-04 21:15:04 +0545

    Add files via upload

---
+++ DoWhileLoop_One.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class DoWhileLoop1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DoWhileLoop_One
+{
+    public static void main(String[] args)
+    {
+        int a = 0;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<=15);
+    }
+}
