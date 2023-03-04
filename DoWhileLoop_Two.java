c98bd44c43684d4f0ec3427c92a1e9b5cffb3da7
Anusha Rai <118553742+Anusharai20@users.noreply.github.com>
2023-03-04 21:15:21 +0545

    Add files via upload

---
+++ DoWhileLoop_Two.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class DoWhileLoop_Two here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DoWhileLoop_Two
+{
+    public static void main(String[]args)
+    {
+        int i = 0; // starting variable
+        do{
+            //body of loop that is executed once no matter what
+            System.out.println("Hello! I am Anusha Rai. ");
+            i++;
+        }
+        //condition that is checked after the one iteration of body
+        while(i<5);
+    }
+}
