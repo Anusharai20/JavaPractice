49cc90ee08dbffb8decb0176ff6042873c4b4210
Anusha Rai <118553742+Anusharai20@users.noreply.github.com>
2023-03-05 21:34:34 +0545

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators{  
+    public static void main(String args[]){  
+        int a=100;  
+        int b=55;  
+        System.out.println(a+b);//155 
+        System.out.println(a-b);//45  
+        System.out.println(a*b);//5500  
+        System.out.println(a/b);//1.82 
+        System.out.println(a%b);//45  
+    }
+} 
