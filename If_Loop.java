da0eec72347d8970a2503886a84aa8b2b5b4e20e
Anusha Rai <118553742+Anusharai20@users.noreply.github.com>
2023-03-08 20:23:39 +0545

    Add files via upload

---
+++ If_Loop.java
@@ -0,0 +1,21 @@
+
+// If statement 
+//syntax
+// if(condition){
+// Statement(s);
+// }
+
+public class If_Loop
+{
+    public static void main(String[] args)
+    {
+        int a, b;
+        a = 12;
+        b = 10;
+        int sum= a + b;
+        System.out.println("The sum of two numbers is: " +sum);
+        if(sum > 18){
+            System.out.println("The sum is greater than 18.");
+        }
+    }
+}
